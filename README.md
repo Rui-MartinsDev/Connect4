5258bc9ea533e78a2d415dae02316ba2105f3966
Rui <111970306+Rui-MartinsDev@users.noreply.github.com>
2024-01-27 12:50:39 +0000

    wining tiles shine

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Simple project of the game Connect 4 with some original functionality!
 Made with vanilla js and vanilla css
 
 Next updates: 
-1. Make an Undo option (Added but with minor bug in the hover of the column that undo)
+1. Mark all the 4 tiles that won the game
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
     /* VANILLA JS CONNECT 4 */
    
-    let playerRed = 'Red', redscore=0;
+    let playerRed = 'Red', redscore=0; //to be used to change the colors of the div classes
     let playerYel ='Yellow', yelscore=0;
+    let pRed='Red', pYel='Yellow'; //to change the scores and update the currplayer
+
     let lastTilePlayed;
+    let tilesPlayed=[]; //to store the values of the tiles played
     let removeHover;
-    let tilesPlayed=[];
     let currPlayer= playerRed;
     const rows=6;
     const columns=7;
@@ -51,12 +53,12 @@
             let tile = document.getElementById(lastTilePlayed)
             if (board[r][c]===`${playerRed}`) {
                 tile.classList.remove('red-piece')
-                currPlayer= playerRed
+                currPlayer= pRed
                 playerTurn.style.color = 'red'
 
             } else if (board[r][c]===`${playerYel}`){
                 tile.classList.remove('yellow-piece')
-                currPlayer=playerYel
+                currPlayer=pYel
                 playerTurn.style.color = 'yellow'
 
             }
@@ -66,15 +68,16 @@
             board[r][c]=' ';
             tile.addEventListener('mouseover', hoverPiece);
             
-
+            runing=true
         }
 
     }
     function displayMenu(){ //open or close menu
         optionsMenu.style.display = optionsMenu.style.display === 'block' ? 'none' : 'block';
     }
-
+    
     function setNames(){ //change the names of the players in the scores
+        
         if (redname.value=='' && yelname.value==''){ // in case no names are assigned
             alert('No names assigned!')
             return
@@ -89,11 +92,15 @@
             alert('Yellow has no name!')
             return
         }
-        playerRed=((redname.value).charAt(0).toUpperCase()+(redname.value).slice(1)).replace(/ne/g, 'nê'); //turns the input into capital
-        playerYel=((yelname.value).charAt(0).toUpperCase()+(yelname.value).slice(1)).replace(/ne/g, 'nê');
-        if (runing) currPlayer=playerRed
-        playerTurn.textContent=`${currPlayer.charAt(0).toUpperCase()+currPlayer.slice(1)}`;
-        score.textContent = `${playerRed}: ${redscore} - ${playerYel}: ${yelscore}`;
+        pRed=((redname.value).charAt(0).toUpperCase()+(redname.value).slice(1)).replace(/ne/g, 'nê'); //turns the input into capital
+        pYel=((yelname.value).charAt(0).toUpperCase()+(yelname.value).slice(1)).replace(/ne/g, 'nê');
+        if (currPlayer==playerRed){
+            playerTurn.textContent=`${pRed.charAt(0).toUpperCase()+pRed.slice(1)}`;   
+        } else {
+            playerTurn.textContent=`${pYel.charAt(0).toUpperCase()+pYel.slice(1)}`;   
+        }
+        
+        score.textContent = `${pRed}: ${redscore} - ${pYel}: ${yelscore}`;
         displayMenu();
     }
 
@@ -134,7 +141,7 @@
     }
 
     function hoverPiece(){
-        
+        if (!runing) return
         let tileColumn= this.id[2] //id is 'r-c'
         let tileRow= currColumn[tileColumn]
         let botTileId = `${tileRow}-${tileColumn}`;
@@ -166,19 +173,17 @@
             colTile.classList.remove(currPlayer);
         }
         
-        lastTilePlayed=`${r}-${c}`; //to shade the last tile played
-        tilesPlayed.push(lastTilePlayed)
+        lastTilePlayed=`${r}-${c}`; //storing the id of the tile played
+        tilesPlayed.push(lastTilePlayed) // this if statement will delete the boxshadow of the previous piece played
         if (tilesPlayed.length>1){
             document.getElementById(tilesPlayed[tilesPlayed.length-2]).style.boxShadow ='';
         }
-
-
 
         if (board[r][c]=== ' ' ){
         board[r][c] = currPlayer;   
         currColumn[c]--;
         let tile = document.getElementById(`${r}-${c}`);
-        tile.style.boxShadow=''
+        tile.style.boxShadow='';
         if (currPlayer == playerRed ){ //the game starts with playerRed = 'Red'
             tile.classList.add('red-piece');
             currPlayer=playerYel;
@@ -196,25 +201,44 @@
         document.getElementById(lastTilePlayed).style.boxShadow ='0 0 10px 10px rgba(0, 0, 0, 0.5)'; //add a effect to the last piece played
 
         checkWin();
-        playerTurn.textContent=`${currPlayer.charAt(0).toUpperCase()+currPlayer.slice(1)}`;   
+        if (currPlayer==playerRed){
+            playerTurn.textContent=`${pRed.charAt(0).toUpperCase()+pRed.slice(1)}`;   
+        } else {
+            playerTurn.textContent=`${pYel.charAt(0).toUpperCase()+pYel.slice(1)}`;   
+        }
     }
 
     
     function checkWin(){
+        
         for (let r=0;r<rows;r++){
             for (let c=0;c<columns;c++){
+                
                 if (board[r][c]!==' '){
                     if (c<columns-3){ //check horizontaly, (dont need to check last 3 columns)
                     
                             if (board[r][c] ===board[r][c+1] && 
                                 board[r][c+1]===board[r][c+2] &&
                                 board[r][c+2] ===board[r][c+3]){
+                                    if (tilesPlayed.length>1){ //deletes the current boxshadow
+                                        document.getElementById(tilesPlayed[tilesPlayed.length-1]).style.boxShadow ='';
+                                    }
+                                    for (let i=0;i<4;i++){ //adds the wining pieces a boxshadow
+                                        let tileId=`${r}-${c+i}`;
+                                        let tile = document.getElementById(tileId)
+                                        if (tile) {
+                                            tile.classList.add('winHover')
+                                            document.getElementById(removeHover).classList.remove(currPlayer);
+                                        } 
+                                        
+                                    }
+                                    
                                     (board[r][c]===playerRed)? redscore++: yelscore++;
                                     //update the score
                                     score.textContent=`${playerRed}: ${redscore} - ${playerYel}: ${yelscore}`
                                     setTimeout(() => {
                                         alert(`${board[r][c]} wins!`)
-                                    }, 100);
+                                    }, 250);
                                     
                                     runing=false;
                                 }
@@ -223,12 +247,23 @@
                         if (board[r][c]===board[r+1][c] &&
                             board[r+1][c]===board[r+2][c] &&
                             board[r+2][c] ===board[r+3][c]){
+                                if (tilesPlayed.length>1){ 
+                                    document.getElementById(tilesPlayed[tilesPlayed.length-1]).style.boxShadow ='';
+                                }
+                                for (let i=0;i<4;i++){
+                                    let tileId=`${r+i}-${c}`;
+                                    let tile = document.getElementById(tileId)
+                                    if (tile) {
+                                        tile.classList.add('winHover')
+                                        document.getElementById(removeHover).classList.remove(currPlayer);
+                                    } 
+                                }
                                 (board[r][c]===playerRed)? redscore++: yelscore++;
                                 //update the score
                                 score.textContent=`${playerRed}: ${redscore} - ${playerYel}: ${yelscore}`
                                 setTimeout(() => {
                                     alert(`${board[r][c]} wins!`)
-                                }, 100);
+                                }, 250);
                                 
                                 runing=false;
                             }
@@ -237,12 +272,24 @@
                         if (board[r][c]===board[r+1][c+1] &&
                             board[r+1][c+1]===board[r+2][c+2] &&
                             board[r+2][c+2]===board[r+3][c+3]){
+                                if (tilesPlayed.length>1){ 
+                                    document.getElementById(tilesPlayed[tilesPlayed.length-1]).style.boxShadow ='';
+                                }
+                                for (let i=0;i<4;i++){
+                                    let tileId=`${r+i}-${c+i}`;
+                                    let tile = document.getElementById(tileId)
+                                    if (tile) {
+                                        tile.classList.add('winHover')
+                                        document.getElementById(removeHover).classList.remove(currPlayer);
+                                    } 
+                                    
+                                }
                                 (board[r][c]===playerRed)? redscore++: yelscore++;
                                 //update the score
                                 score.textContent=`${playerRed}: ${redscore} - ${playerYel}: ${yelscore}`
                                 setTimeout(() => {
                                     alert(`${board[r][c]} wins!`)
-                                }, 100);
+                                }, 250);
                                 
                                 runing=false;
                             }
@@ -251,12 +298,24 @@
                         if (board[r][c]===board[r-1][c+1] &&
                             board[r-1][c+1]===board[r-2][c+2] &&
                             board[r-2][c+2]===board[r-3][c+3]){
+                                if (tilesPlayed.length>1){ 
+                                    document.getElementById(tilesPlayed[tilesPlayed.length-1]).style.boxShadow ='';
+                                }
+                                for (let i=0;i<4;i++){
+                                    let tileId=`${r-i}-${c+i}`;
+                                    let tile = document.getElementById(tileId)
+                                    if (tile) {
+                                        tile.classList.add('winHover')
+                                        document.getElementById(removeHover).classList.remove(currPlayer);
+                                    } 
+                                    
+                                }
                                 (board[r][c]===playerRed)? redscore++: yelscore++;
                                 //update the score
                                 score.textContent=`${playerRed}: ${redscore} - ${playerYel}: ${yelscore}`
                                 setTimeout(() => {
                                     alert(`${board[r][c]} wins!`)
-                                }, 100);
+                                }, 250);
                                 
                                 runing=false;
                             }
@@ -264,7 +323,7 @@
                 }
             }
         }
-        if (!(board.flat().includes(' '))) {
+        if (!(board.flat().includes(' '))) { //draw
             setTimeout(() => {
                 alert(`Draw!`)
             }, 100);
